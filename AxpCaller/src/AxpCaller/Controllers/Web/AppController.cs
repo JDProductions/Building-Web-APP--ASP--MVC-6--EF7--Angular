060393a328b012cf5b31d6f6ea7e250a584bcde3
JamesDuBois1994 <jamesdubois@oeconnection.com>
2016-01-22 16:15:26 -0500

    AxpCaller

--- AxpCaller/src/AxpCaller/Controllers/Web/AppController.cs
+++ AxpCaller/src/AxpCaller/Controllers/Web/AppController.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using System.ComponentModel.Design;
+﻿using AxpCaller.ViewModels;
+using Microsoft.AspNet.Mvc;
 
 namespace AxpCaller.Controllers.Web
 {
-    public class AppController
+    public class AppController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+
+       //[HttpPost]
+        public IActionResult Activate (ActivateViewModel aModel)
+        {
+            return View();
+            
+        }
+
     }
 
 }
---
+++ AxpCaller/src/AxpCaller/ViewModels/ActivateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AxpCaller.ViewModels
+{
+    public class ActivateViewModel
+    {
+        [Required]
+        public string CompanyID { get; set; }
+    }
+
+}
