6fd8d2268d8cd2d5205e41fb752ddf720f8eb9c3
JamesDuBois1994 <jamesdubois@oeconnection.com>
2016-01-22 13:45:26 -0500

    First ApxCaller commit

---
+++ AxpCaller/src/AxpCaller/Controllers/Web/AppController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace AxpCaller.Controllers.Web
+{
+    public class AppController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
