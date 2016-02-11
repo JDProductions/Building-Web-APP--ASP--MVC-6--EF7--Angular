0642441cffbb7e8bac70537b1e5f5a46b92ca04c
JamesDuBois1994 <jamesdubois@oeconnection.com>
2016-02-11 10:47:47 -0500

    Added CompanyModel
    New Model

--- AxpCallerRewrite/src/AxpCallerRewrite/Models/CompanyModel.cs
+++ AxpCallerRewrite/src/AxpCallerRewrite/Models/CompanyModel.cs
@@ -7,6 +7,15 @@ namespace AxpCallerRewrite.Models
 {
     public class CompanyModel
     {
-        
+        public string CompanyName { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string CompanyType { get; set; }
+
     }
 }
