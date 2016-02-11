569033eea25c0751771cc426e9037f5dfe7ae10d
kingw12 <Wesley.King@oeconnection.com>
2016-02-11 13:28:13 -0500

    LegacyHelper - Process SqlData
    Process the SQL data and prepare to be able to add data to list

--- AxpCallerRewrite/src/AxpCallerRewrite/Concrete/LegacyHelper.cs
+++ AxpCallerRewrite/src/AxpCallerRewrite/Concrete/LegacyHelper.cs
@@ -23,18 +23,40 @@ namespace AxpCallerRewrite.Concrete
         {
             using (SqlDataReader reader = repo.GetCompanyTypes().ExecuteReader())
             {
+                //a list to keep track of the rows and their values
+                List<Object> rowList = new List<Object>();
+
                 while (reader.Read())
                 {
-                    // write the data on to the screen
-                    Console.WriteLine(String.Format("{0} \t | {1} \t | {2} \t | {3}",
-                    // call the objects from their index
-                    reader[0], reader[1], reader[2], reader[3]));
+                    //creates an array obect the size of the number of columns
+                    object[] values = new object[reader.FieldCount];
+                    //inserts the column values into the object array for the current row
+                    reader.GetValues(values);
+                    //add the row to the list
+                    rowList.Add(values);
+                }
+
+                foreach (object[] row in rowList)
+                {
+                    // Create a string array large enough to hold all the column values in this array
+                    string[] rowInfo = new string[row.Length];
+                    // Create a column index into the array
+                    int columnIndex = 0;
+                    // Now process each column value
+                    foreach (object column in row)
+                    {
+                        // Convert the value to a string and stick it in the string array
+                        rowInfo[columnIndex++] = Convert.ToString(column);
+
+                    }
+
+                    //Do stuff with data depending on what is given from the database
+                    //Example: CompanyTypeList.Add(rowInfo[0])
                 }
             }
         }
     }
-
-    }
+}
 
 
 
